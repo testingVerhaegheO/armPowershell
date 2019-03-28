f5e2ea0ee771988779265fa81a2731ca10eaf4f7
testingVerhaegheO <olivier.verhaeghe@delaware.pro>
2019-03-28 09:51:56 +0100

    armPowershell001

---
+++ loadComponentsToAzure.ps1
@@ -0,0 +1,24 @@
+﻿#Install-Module AzureRM
+#Import-Module AzureRM
+
+#Install-Module -Name Az -AllowClobber
+#Import-Module -Name Az
+
+#Connect-AzAccount
+
+$starttime= get-date
+
+
+$resourceGroupName = ‘testingVerhaegheO03’
+$location = ‘west-europe’
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -templateUri C:\Users\olivier.verhaeghe\Desktop\AKS\main.json
+
+$stoptime = Get-Date
+$time = $stoptime - $starttime
+Write-Host $time
+
+#via github
+#New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    #-templateUri https://raw.githubusercontent.com/testingVerhaegheO/armdeploy/master/main.json
---
+++ webapp+sqlserver+appInsights.ps1
@@ -0,0 +1,25 @@
+﻿#Install-Module AzureRM
+#Import-Module AzureRM
+
+#Install-Module -Name Az -AllowClobber
+#Import-Module -Name Az
+
+#Connect-AzAccount
+
+$starttime= get-date
+
+
+$resourceGroupName = ‘testingVerhaegheO02’
+$location = ‘west-europe’
+
+New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    -templateUri C:\Users\olivier.verhaeghe\Desktop\webapp+sqlserver+appInsights\main.json
+
+
+$stoptime = Get-Date
+$time = $stoptime - $starttime
+Write-Host $time
+
+#via github
+#New-AzResourceGroupDeployment -ResourceGroupName $resourceGroupName `
+    #-templateUri https://raw.githubusercontent.com/testingVerhaegheO/armdeploy/master/main.json
